acfefbb3b3fa0c6287dd94b252e7071c61d72326
ysyms <ysyms@ysyms.local>
2023-10-18 18:54:36 +0800

    描述你的更改

--- src/dashboard_backend/main.mo
+++ src/dashboard_backend/main.mo
@@ -1,5 +1,19 @@
+import Principal "mo:base/Principal";
+import Text "mo:base/Text";
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
+    type ExternalCanister = actor {
+        icrc1_balance_of: shared query ({ owner: Principal; subaccount: ?[Nat8] }) -> async Nat;
+        icrc1_name:shared query()->async Text
+    };
+
+    public shared func icrc1_token_info() : async (Nat,Text) {
+        
+        let token_canister: ExternalCanister = actor "zfcdd-tqaaa-aaaaq-aaaga-cai";
+        let wallet_pid = Principal.fromText("vfhdx-67tj7-akypt-6a6fx-yqzlr-3om4p-a7cem-qkozv-76oie-ghfgv-jae");
+
+        let balance : Nat = await token_canister.icrc1_balance_of({owner= wallet_pid;subaccount= null});
+        let name:Text=await token_canister.icrc1_name();
+        return (balance,name);
+    };
 };
--- src/dashboard_frontend/src/index.js
+++ src/dashboard_frontend/src/index.js
@@ -9,11 +9,24 @@ document.querySelector("form").addEventListener("submit", async (e) => {
   button.setAttribute("disabled", true);
 
   // Interact with foo actor, calling the greet method
-  const greeting = await dashboard_backend.greet(name);
+  const icrc1_token_info = await dashboard_backend.icrc1_token_info();
 
   button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
+  document.getElementById("greeting").innerText = convertE8sToNumber(icrc1_token_info[1]+"    "+icrc1_token_info[0]);;
 
   return false;
 });
+function convertE8sToNumber(e8sValue) {
+  // assuming e8sValue is a string, you might need to adjust the conversion logic based on actual data type
+  // and format of e8sValue
+  
+  const scaleFactor = Math.pow(10, 8); // e8s has a scale factor of 10^8
+  return parseFloat(e8sValue) / scaleFactor;
+}
+// document.getElementById("对比").addEventListener("click", async ()=>{
+//   let first_num=parseInt(document.getElementById("num1").value,0)
+//   let last_num=parseInt(document.getElementById("num2").value,0)
+//   let bigNum=await dashboard_backend.bigInt(first_num,last_num);
+//   document.getElementById("big").textContent=bigNum;
+// })
